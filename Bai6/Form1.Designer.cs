dc6b61fc3264ac1add39058ce941e30fb034e2d7
JustRogueChicken <164870328+JustRogueChicken@users.noreply.github.com>
2024-06-15 21:27:01 +0700

    Add files via upload

---
+++ Bai6/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Bai6
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "URL";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Username";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(32, 158);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 16);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Password";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(610, 74);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 49);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "LOGIN";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(610, 173);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(131, 49);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "GET USER INFO";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(123, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(618, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(123, 86);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(414, 22);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(123, 155);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(414, 22);
+            this.textBox3.TabIndex = 5;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(35, 243);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(706, 195);
+            this.textBox4.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2; // New button for GET request
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
---
+++ Bai6/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using Newtonsoft.Json.Linq;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Bai6
+{
+    public partial class Form1 : Form
+    {
+        private string tokenType;
+        private string accessToken;
+
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Click += new EventHandler(this.Button1_Click);
+            button2.Click += new EventHandler(this.Button2_Click); 
+        }
+
+        private async void Button1_Click(object sender, EventArgs e)
+        {
+            string username = textBox2.Text;
+            string password = textBox3.Text;
+
+            var url = textBox1.Text = "https://nt106.uitiot.vn/auth/token";
+
+            using (var client = new HttpClient())
+            {
+                var content = new MultipartFormDataContent
+                {
+                    { new StringContent(username), "username" },
+                    { new StringContent(password), "password" }
+                };
+
+                var response = await client.PostAsync(url, content);
+                var responseString = await response.Content.ReadAsStringAsync();
+                var responseObject = JObject.Parse(responseString);
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    var detail = responseObject["detail"]?.ToString();
+                    textBox4.Text = $"Detail: {detail}";
+                    return;
+                }
+               
+
+                tokenType = responseObject["token_type"]?.ToString();
+                accessToken = responseObject["access_token"]?.ToString();
+                textBox4.Text = $"Token Type: {tokenType}\nAccess Token: {accessToken}\nĐăng nhập thành công";
+                client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", accessToken);
+            }
+        }
+
+        private async void Button2_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(tokenType) || string.IsNullOrEmpty(accessToken))
+            {
+                textBox4.Text = "Vui lòng đăng nhập trước.";
+                return;
+            }
+
+            var getUserUrl = "https://nt106.uitiot.vn/api/v1/user/me";
+
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue(tokenType, accessToken);
+
+                var getUserResponse = await client.GetAsync(getUserUrl);
+                var getUserResponseString = await getUserResponse.Content.ReadAsStringAsync();
+
+                if (getUserResponse.IsSuccessStatusCode)
+                {
+                    var userObject = JObject.Parse(getUserResponseString);
+                    var userId = userObject["id"]?.ToString();
+                    var fullName = userObject["full_name"]?.ToString();
+                    var email = userObject["email"]?.ToString();
+
+                    if (userId != null)
+                    {
+                        textBox4.AppendText($"User ID: {userId}\n");
+                    }
+                    if (fullName != null)
+                    {
+                        textBox4.AppendText($"Full Name: {fullName}\n");
+                    }
+                    if (email != null)
+                    {
+                        textBox4.AppendText($"Email: {email}\n");
+                    }
+
+                    textBox4.AppendText("\nĐăng nhập thành công!\n");
+                }
+                else
+                {
+                    textBox4.AppendText($"Error retrieving user information: {getUserResponse.StatusCode}\n");
+                }
+
+            }
+        }
+
+        
+    }
+    }
+ 
+
